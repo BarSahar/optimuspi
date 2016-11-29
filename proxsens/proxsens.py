ff831943f189e7a0225cc1acb0c819ef5691eaba
BarSahar <bsahar1990@gmail.com>
2016-11-29 15:13:00 +0200

    asd

--- proxsens/proxsens.py
+++ proxsens/proxsens.py
@@ -2,32 +2,69 @@
 import time
 GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
-ECHO = 22
+def getDist():
+    #{
+    TRIG = 23
+    ECHO = 22
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    GPIO.output(TRIG, False)
+    time.sleep(1)
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    GPIO.cleanup()
+    return distance;
+    #}
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+def moveForward():
+    #{
+    A1 = 26
+    A2 = 27
+    B1 = 24
+    B2 = 25
+    GPIO.setup(A1,GPIO.OUT)
+    GPIO.setup(A2,GPIO.OUT)
+    GPIO.setup(B1,GPIO.OUT)
+    GPIO.setup(B2,GPIO.OUT)
+    GPIO.output(A1, 1)
+    GPIO.output(A2, 0)
+    GPIO.output(B1, 1)
+    GPIO.output(B2, 0)
+    #time.sleep(3)
+    GPIO.cleanup()
+    return;
+    #}
 
-GPIO.output(TRIG, False)
+def stop():
+    #{
+    A1 = 26
+    A2 = 27
+    B1 = 24
+    B2 = 25
+    GPIO.setup(A1,GPIO.OUT)
+    GPIO.setup(A2,GPIO.OUT)
+    GPIO.setup(B1,GPIO.OUT)
+    GPIO.setup(B2,GPIO.OUT)
+    GPIO.output(A1, 0)
+    GPIO.output(A2, 0)
+    GPIO.output(B1, 0)
+    GPIO.output(B2, 0)
+    GPIO.cleanup()
+    return;
+    #}
 
-time.sleep(1)
-
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
-
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
-
-GPIO.cleanup()
+if getDist()<20:
+    print "HERE WE GO!!!!!!"
+    moveForward()
+    while (getDist() <20):
+        print "STILL ROLLING!"
+    stop()
+    print "tired"
