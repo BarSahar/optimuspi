9b85203a9a35defb7ff7a14e2120ffe240184db3
USER <USER@DESKTOP-L7GO5KG>
2016-12-15 15:56:28 +0200

    asd

--- proxsens/proxsens.py
+++ proxsens/proxsens.py
@@ -164,8 +164,8 @@ def moveForward():
 	global counterright_limit
 	global counterleft_limit
 	globalinit()
-	counterright_limit=100
-	counterleft_limit=100
+	counterright_limit=150
+	counterleft_limit=150
 	GPIO.setmode(GPIO.BCM)
 	GPIO.add_event_detect(21,GPIO.RISING,callback=addright)
 	GPIO.add_event_detect(20,GPIO.RISING,callback=addleft)
