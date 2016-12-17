98b55bafcd940b3eccd50e0f89dddee1b1e82963
Bar <bsahar1990@gmail.com>
2016-12-17 11:53:32 +0200

    as

--- proxsens/laser.py
+++ proxsens/laser.py
@@ -4,9 +4,13 @@ import picamera.array
 import cv2
 import numpy as np
 import math
- 
-#variables
+import distConst
 
+#Calibration Constants 
+
+#DistConsts = np.loadtxt()
+
+#Calibration
 with picamera.PiCamera() as camera:
 	with picamera.array.PiRGBArray(camera) as stream:
 		for j in range(8):
@@ -33,3 +37,36 @@ with picamera.PiCamera() as camera:
 
 	        #print "\033[12;0H" + "the dot is " + str(obj_dist) + "cm  away"s
 		camera.close()
+
+
+isCalibrated = False
+
+#CalcDistCalibration
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        x = input("system ready")
+        camera.capture(stream, format='bgr')
+        image = stream.array
+        num = (image[...,...,1] > 200)
+        y_vals = [0] * 640
+        for i in range(640) :
+            y_vals[i] = abs(np.median(num[:,i].nonzero())-240)
+        dist = abs(y_vals - 240) # distance of dot from center y_axis only
+        pixelDist = [0] * 640
+
+        for i in range(640) :
+            if DistConsts[i] != 0 :
+                pixelDist[i] = y_vals[i]*DistConsts[i].Slope + DistConsts[i].Intercept
+            y_vals[i] = abs(np.median(num[:,i].nonzero())-240)        
+
+        
+	    # work out distance using D = h/tan(theta)
+
+	    #theta =0.0011450*dist + 0.0154
+	    #tan_theta = math.tan(theta)
+
+		#if tan_theta > 0: # bit of error checking
+		#obj_dist =  int(5.33 / tan_theta)
+
+	    #print "\033[12;0H" + "the dot is " + str(obj_dist) + "cm  away"s
+
