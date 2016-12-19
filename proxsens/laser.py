665c09b7cf0c1427fdf7ea680ce62f63ae052d97
BarSahar <bsahar1990@gmail.com>
2016-12-19 15:53:58 +0200

    asd

--- proxsens/laser.py
+++ proxsens/laser.py
@@ -19,7 +19,7 @@ def getLaserDistArr():
             num = (image[...,...,1] > 200)
             y_vals = [0] * 640
             for i in range(200,400) :
-                y_vals[i] = abs(np.median(num[:,i].nonzero())-240)
+                y_vals[i] = abs(np.nanmedian(num[:,i].nonzero())-240)
             dist = abs(y_vals - 240) # distance of dot from center y_axis only
 	        #save the dists of all..... stuff
         camera.close()
