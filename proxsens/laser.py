545192b9fd4186fcb9ef6b5a865de5d274b1e12f
Bar <bsahar1990@gmail.com>
2016-12-21 19:43:59 +0200

    asd

--- proxsens/laser.py
+++ proxsens/laser.py
@@ -14,21 +14,21 @@ GPIO.setwarnings(False)
 DistConstArr = np.empty(640,dtype=DistConst)
 distH = 5
 initialD = 285
-isCalibrated = False
 
 
-#Calibration
+#Function to detect laser line from camera and return array of y distances from camera horizon per column
+#Since camera resolution is 640*480, an array of 680 values is returned
 def getLaserDistArr():
     with picamera.PiCamera() as camera:
         with picamera.array.PiRGBArray(camera) as stream:
-            camera.capture(stream, format='bgr')
+            camera.capture(stream, format='bgr') #take a photo
             image = stream.array
-            num = (image[...,...,1] > 200)
+            num = (image[...,...,1] > 200) #only marks pixels of bright green color
             y_vals = [np.nan]*640
             for i in range(200,400) :
                 x = num[:,i].nonzero()
                 if len(x) != 0 :
-                    y_vals[i] = abs(np.median(x)-240)
+                    y_vals[i] = abs(np.median(x)-240) #in case of multiple pixles per column, a median of dais pixel is computed
         camera.close()
     return y_vals
 
@@ -37,26 +37,26 @@ def cali():
     R1 = 18 # RELAY PIN	
     GPIO.setup(R1,GPIO.OUT)
     pixelDist = []
-    theta = np.empty(10) #instantaniates as D in cm, later converted to theta
-    theta.fill(initialD)
+    theta = np.empty(10) #an array of theta per distance from wall
+    theta.fill(initialD) #the array instantaniates as D in cm and than
+                         #converted to angle via artctan(h/D)
     for x in range(10):
             GPIO.output(R1, True) # laser on
             pixelDist.append(getLaserDistArr())
-            theta[x] = math.atan(distH/(theta[x]-30*x))
+            theta[x] = math.atan(distH/(theta[x]-30*x)) #D in cm converted to theta
             GPIO.output(R1, False) #laser off
             print ("ended loop" + str(x))
             #move30cm()
             time.sleep(0.5)
     
-    np_pixelDist = np.asarray(pixelDist)
+    np_pixelDist = np.asarray(pixelDist) #pixelDist matrix converted to numpy matrix - for performance
+    #take every column and compute slope and offset from multiple readings
     for i in range(200,400) :
-        x = np_pixelDist[:,i]
-        mask = ~np.isnan(x)
+        x = np_pixelDist[:,i] 
+        mask = ~np.isnan(x) #mask = ignore any 'nan' values in the matrix
         slope, intercept, r_value, p_value, std_err = stats.mstats.linregress(x[mask],theta[mask])
-        DistConstArr[i] = DistConst(slope,intercept)
+        DistConstArr[i] = DistConst(slope,intercept) #add computer values to distance constants array
     np.save('consts.txt', DistConstArr)
-    isCalibrated=True
-##clac here
 
 cali()
 
