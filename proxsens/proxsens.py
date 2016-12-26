90395346e2eef2c39dcf18cfd3479a3959ff68a1
BarSahar <bsahar1990@gmail.com>
2016-12-26 17:28:09 +0200

    asd

--- proxsens/proxsens.py
+++ proxsens/proxsens.py
@@ -27,25 +27,42 @@ counterright=0
 counterleft_limit=0
 counterright_limit=0
 
-def getDist():
+TRIG_pin = 23
+ECHO_pin = 22
+
+def getProxDist():
     GPIO.setmode(GPIO.BCM)
     TRIG = 23
     ECHO = 22
-    GPIO.setup(TRIG,GPIO.OUT)
-    GPIO.setup(ECHO,GPIO.IN)
-    GPIO.output(TRIG, False)
-    time.sleep(1)
-    GPIO.output(TRIG, True)
+    GPIO.setup(TRIG_pin,GPIO.OUT)
+    GPIO.setup(ECHO_pin,GPIO.IN)
+    GPIO.output(TRIG_pin, False)
+
+    distance1=measureProx()
+    time.sleep(0.1)
+    distance2=measureProx()
+    time.sleep(0.1)
+    distance3=measureProx()
+    distance = (distance1 + distance2 + distance3)/3
+    return distance
+
+def measureProx():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TRIG_pin,GPIO.OUT)
+    GPIO.setup(ECHO_pin,GPIO.IN)
+    GPIO.output(TRIG_pin, False)
+
+    GPIO.output(TRIG_pin, True)
     time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    while GPIO.input(ECHO)==0:
+    GPIO.output(TRIG_pin, False)
+    while GPIO.input(ECHO_pin)==0:
         pulse_start = time.time()
-    while GPIO.input(ECHO)==1:
+    while GPIO.input(ECHO_pin)==1:
         pulse_end = time.time()
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
-    distance = round(distance, 2)
     return distance
+
 def stop():
     GPIO.setmode(GPIO.BCM)
     A1 = 26
