e5696566297a8832a91e83571b650e50489ad4d1
USER <USER@DESKTOP-L7GO5KG>
2016-12-27 13:36:46 +0200

    sens.getProxDist()

--- proxsens/outliner.py
+++ proxsens/outliner.py
@@ -12,32 +12,57 @@ def outline():
 	while True:
 		if (home==cposition) and (~first):
 			return True
-
 		first=False
 
-		if sens.getDist>33:  #wall too far on the right
-			if sens.getLaserDist>45: #enough room to enter "door size"
+
+		if sens.getLaserDist()>67 and sens.getProxDist()>60: #door or slit
+			if doorcheck():
+				sens.turnright()
+		elif sens.getProxDist()>33 and sens.getProxDist()<37:  #wall too far on the right
 				sens.turnright()
 				sens.moveForward()
 				updateCposition()
-			elif sens.getLaserDist<13.5: # wall infront
+				sens.turnleft()
+		elif sens.getProxDist()<13.5: # wall too close on the right
 				sens.turnleft()
 				sens.moveForward()
 				updateCposition()
-			else:
-				sens.moveForward() #skiping slit
-				updateCposition()
-		elif sens.getDist<13.5:
-			sens.turnleft()
-			if sens.getLaserDist>33:
-				sens.moveForward() 
-				updateCposition() 
 				sens.turnright()
-				#ignoring getLaserDist <13.5 for now
+		else:                         # wall on the right is OK
+			if sens.getLaserDist()>33:
+				sens.moveForward()
+				updateCposition()
+			else:
+				sens.turnleft()
+
 
 
 	
 	
 def updateCposition():
 	cposition=tuple(map(op.add, cposition,sens.cosmos[sens.dir]))
-	
+
+def ifathome():
+	if (home==cposition):
+		return True
+	else:
+		return False
+
+def doorcheck():
+	for x in range(4):
+		if sens.getProxDist()>60 and sens.getLaserDist()>17:
+			sens.moveForward()
+			updateCposition()
+			count=count+1
+			if ifathome():
+				return False
+		else:
+			return False
+
+		sens.turnleft()
+		sens.turnleft()
+		for x in range(4):
+			sens.moveForward()
+		sens.turnright()	
+		sens.turnright()
+		return True
