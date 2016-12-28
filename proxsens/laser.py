75d1e364a1e67f54a46c0b014c4e78b0067d85ba
BarSahar <bsahar1990@gmail.com>
2016-12-28 18:13:39 +0200

    asd

--- proxsens/laser.py
+++ proxsens/laser.py
@@ -4,7 +4,6 @@ import picamera.array
 import numpy as np
 from math import tan
 import cv2
-from cv2 import cv
 GPIO.setwarnings(False)
 
 def getPicture():
@@ -25,7 +24,7 @@ def getLaserDist():
     GPIO.output(R1, False) #laser off
     
 
-    hsv_img = cv2.cvtColor(image, cv.CV_BGR2HSV)
+    hsv_img = cv2.cvtColor(image, cv2.CV_BGR2HSV)
 
     LASER_MIN = np.array([120, 100, 45.1],np.uint8)
     LASER_MAX = np.array([120, 100, 100],np.uint8)
@@ -33,7 +32,7 @@ def getLaserDist():
     frame_threshed = cv2.inRange(hsv_img, LASER_MIN, LASER_MAX)
 
     #cv.InRangeS(hsv_img,cv.Scalar(5, 50, 50),cv.Scalar(15, 255, 255),frame_threshed)    # Select a range of yellow color
-    src = cv.fromarray(frame_threshed)
+    src = cv2.fromarray(frame_threshed)
     #rect = cv.BoundingRect(frame_threshed, update=0)
 
     leftmost=0
@@ -44,21 +43,21 @@ def getLaserDist():
     laserx = 0
     lasery = 0
     for i in range(src.width):
-        col=cv.GetCol(src,i)
-        if cv.Sum(col)[0]!=0.0:
+        col=cv2.GetCol(src,i)
+        if cv2.Sum(col)[0]!=0.0:
             rightmost=i
             if temp==0:
                 leftmost=i
                 temp=1
     for i in range(src.height):
-        row=cv.GetRow(src,i)
-        if cv.Sum(row)[0]!=0.0:
+        row=cv2.GetRow(src,i)
+        if cv2.Sum(row)[0]!=0.0:
             bottommost=i
             if temp==1:
                 topmost=i
                 temp=2
 
-    laserx=cv.Round((rightmost+leftmost)/2)
-    lasery=cv.Round((bottommost+topmost)/2)
+    laserx=cv2.Round((rightmost+leftmost)/2)
+    lasery=cv2.Round((bottommost+topmost)/2)
     #return (leftmost,rightmost,topmost,bottommost)
     return laserx, lasery
