6065a1e4da55798e8f16a71e482a1a14d5a76801
Avi <USER@DESKTOP-L7GO5KG>
2017-01-05 17:30:39 +0200

    blockpoints

--- proxsens/uncheckPointFinder.py
+++ proxsens/uncheckPointFinder.py
@@ -13,9 +13,11 @@ def uncheckFinder(start,graph):
 	q=getNeighbours(start,graph)
 
 	flag=False
+	point
+
 	while q:
-		point=q.pop([0])
-		if point is status.uncheck:
+		point=q.pop(0)
+		if graph[point[0]][point[1]] == status.uncheck:
 			flag=True
 			break
 		q=q+getNeighbours(point,graph)
@@ -24,12 +26,17 @@ def uncheckFinder(start,graph):
 		path=pathfinder(start,point)
 		return (path,point)
 	else:
-		return False
+		return ((-1,-1),(-1,-1)) 
 
 
+def markblockpoint(pointlist,graph):
+	for point in pointlist:
+		if graph[point[0]][point[1]] == status.block:
+			blockpoints.append(point)
 
 def getNeighbours(point,graph):
 
+	checkedpoints.append(point)
 	rightP=tuple(map(op.add, point,(1,0)))
 	frontP=tuple(map(op.add, point,(0,1)))
 	leftP=tuple(map(op.add, point,(-1,0)))
@@ -37,33 +44,50 @@ def getNeighbours(point,graph):
 
 	templist=[rightP,frontP,leftP,backP]
 
-	markblockpoint(templist)
+	markblockpoint(templist,graph)
 	anslist=[]
-
 	for temp in templist:
 		if temp not in blockpoints and temp not in checkedpoints:
 			anslist.append(temp)
-			parentlist.append(temp,point)
+			parentlist.append((temp,point))
+	return anslis
 
 
-	return anslist
+#	unmark=GiveUncheckPoints(templist)
+#	unblock=GiveUnblockPoints(unmark,graph)
+#	markPoints(unblock)
+#	return unblock
 	
-	
+def markPoints(points):
+	for point in points:
+		checkedpoints.append(point)
+
+
+def GiveUncheckPoints(pointlist):
+	list=[]
+	for point in pointlist:
+			if point not in checkedpoints:
+				list.append(point)
+	return list
 
 def isuncheckPoint(point,graph):
-	if graph[point[0]][point[1]] is status.uncheck:
+	if graph[point[0]][point[1]] == status.uncheck:
 		return True
 	else:
 		return False
 
 
-def markblockpoint(pointlist,graph):
+def GiveUnblockPoints(pointlist,graph):
+	lst=[]
 	for point in pointlist:
-		if graph[point[0]][point[1]] is status.block:
-			blocklist.append(point)
-			checkedpoints.append(point)
+		if graph[point[0]][point[1]] == status.block:
+			blockpoints.append(point)
 		else:
-			checkedpoints.append(point)
+			lst.append(point)
+	return lst
+		
+
+
 
 #(child,parent)
 def pathfinder(start,end):
@@ -112,10 +136,13 @@ def main():
 	grid[17][1]=status.uncheck
 	grid[17][17]=status.uncheck
 
-	param=uncheckFinder(start,grid)
-	endpoint=param[1]
+	
 
-	print(str(endpoint))
+	path,endpoint=uncheckFinder(start,grid)
+	
+	print("start: "+str(start))
+	print("path: "+str(path))
+	print("end point:"+str(endpoint))
 
 
 main()
