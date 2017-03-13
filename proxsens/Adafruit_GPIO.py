8ddb9b869587c828691142c633f8747359b3d6d7
Avi <USER@DESKTOP-L7GO5KG>
2017-03-13 16:33:29 +0200

    sad

--- proxsens/Adafruit_GPIO.py
+++ proxsens/Adafruit_GPIO.py
@@ -1,219 +1,200 @@
-# The MIT License (MIT)
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+# Based on Adafruit_I2C.py created by Kevin Townsend.
 #
-# Copyright (c) 2017, Jack Weatherilt
-# All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#  1. Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
-#
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-import math
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import logging
+import subprocess
 
-# The same address is used for both the magnetometer and accelerometer, but
-# each has their own variable, to avoid confusion.
-LSM9DS0_MAG_ADDRESS     = 0x1D
-LSM9DS0_ACCEL_ADDRESS	= 0x1D
-LSM9DS0_GYRO_ADDRESS    = 0x6B
-                                                                                
-# LSM9DS0 gyrometer registers
-LSM9DS0_WHO_AM_I_G      = 0x0F
-LSM9DS0_CTRL_REG1_G     = 0x20
-LSM9DS0_CTRL_REG3_G     = 0x22
-LSM9DS0_CTRL_REG4_G     = 0x23
-LSM9DS0_OUT_X_L_G       = 0x28
-LSM9DS0_OUT_X_H_G       = 0x29
-LSM9DS0_OUT_Y_L_G       = 0x2A
-LSM9DS0_OUT_Y_H_G       = 0x2B
-LSM9DS0_OUT_Z_L_G       = 0x2C
-LSM9DS0_OUT_Z_H_G       = 0x2D
+import Adafruit_GPIO.Platform as Platform
 
-# 2D gyroscope low-high register tuple
-LSM9DS0_OUT_XYZ_LH_G    = ((LSM9DS0_OUT_X_L_G, LSM9DS0_OUT_X_H_G),
-                           (LSM9DS0_OUT_Y_L_G, LSM9DS0_OUT_Y_H_G),
-                           (LSM9DS0_OUT_Z_L_G, LSM9DS0_OUT_Z_H_G))
 
-# LSM9DS0 temperature addresses
-LSM9DS0_OUT_TEMP_L_XM   = 0x05
-LSM9DS0_OUT_TEMP_H_XM   = 0x06
+def reverseByteOrder(data):
+    """Reverses the byte order of an int (16-bit) or long (32-bit) value."""
+    # Courtesy Vishal Sapre
+    byteCount = len(hex(data)[2:].replace('L','')[::2])
+    val       = 0
+    for i in range(byteCount):
+        val    = (val << 8) | (data & 0xff)
+        data >>= 8
+    return val
 
-# Temperature low-high register tuple
-LSM9DS0_OUT_TEMP_LH = (LSM9DS0_OUT_TEMP_L_XM, LSM9DS0_OUT_TEMP_H_XM)
+def get_default_bus():
+    """Return the default bus number based on the device platform.  For a
+    Raspberry Pi either bus 0 or 1 (based on the Pi revision) will be returned.
+    For a Beaglebone Black the first user accessible bus, 1, will be returned.
+    """
+    plat = Platform.platform_detect()
+    if plat == Platform.RASPBERRY_PI:
+        if Platform.pi_revision() == 1:
+            # Revision 1 Pi uses I2C bus 0.
+            return 0
+        else:
+            # Revision 2 Pi uses I2C bus 1.
+            return 1
+    elif plat == Platform.BEAGLEBONE_BLACK:
+        # Beaglebone Black has multiple I2C buses, default to 1 (P9_19 and P9_20).
+        return 1
+    else:
+        raise RuntimeError('Could not determine default I2C bus for platform.')
 
-# Magnetometer addresses
-LSM9DS0_STATUS_REG_M    = 0x07
-LSM9DS0_OUT_X_L_M       = 0x08
-LSM9DS0_OUT_X_H_M       = 0x09
-LSM9DS0_OUT_Y_L_M       = 0x0A
-LSM9DS0_OUT_Y_H_M       = 0x0B
-LSM9DS0_OUT_Z_L_M       = 0x0C
-LSM9DS0_OUT_Z_H_M       = 0x0D
+def get_i2c_device(address, busnum=None, i2c_interface=None, **kwargs):
+    """Return an I2C device for the specified address and on the specified bus.
+    If busnum isn't specified, the default I2C bus for the platform will attempt
+    to be detected.
+    """
+    if busnum is None:
+        busnum = get_default_bus()
+    return Device(address, busnum, i2c_interface, **kwargs)
 
-# 2D magnetometer low-high register tuple
-LSM9DS0_OUT_XYZ_LH_M    = ((LSM9DS0_OUT_X_L_M, LSM9DS0_OUT_X_H_M),
-                           (LSM9DS0_OUT_Y_L_M, LSM9DS0_OUT_Y_H_M),
-                           (LSM9DS0_OUT_Z_L_M, LSM9DS0_OUT_Z_H_M))
+def require_repeated_start():
+    """Enable repeated start conditions for I2C register reads.  This is the
+    normal behavior for I2C, however on some platforms like the Raspberry Pi
+    there are bugs which disable repeated starts unless explicitly enabled with
+    this function.  See this thread for more details:
+      http://www.raspberrypi.org/forums/viewtopic.php?f=44&t=15840
+    """
+    plat = Platform.platform_detect()
+    if plat == Platform.RASPBERRY_PI:
+        # On the Raspberry Pi there is a bug where register reads don't send a
+        # repeated start condition like the kernel smbus I2C driver functions
+        # define.  As a workaround this bit in the BCM2708 driver sysfs tree can
+        # be changed to enable I2C repeated starts.
+        subprocess.check_call('chmod 666 /sys/module/i2c_bcm2708/parameters/combined', shell=True)
+        subprocess.check_call('echo -n 1 > /sys/module/i2c_bcm2708/parameters/combined', shell=True)
+    # Other platforms are a no-op because they (presumably) have the correct
+    # behavior and send repeated starts.
 
-# Shared (mag and accel) addresses
-LSM9DS0_WHO_AM_I_XM     = 0x0F
-LSM9DS0_INT_CTRL_REG_M  = 0x12
-LSM9DS0_INT_SRC_REG_M   = 0x13
-LSM9DS0_CTRL_REG1_XM    = 0x20
-LSM9DS0_CTRL_REG2_XM    = 0x21
-LSM9DS0_CTRL_REG5_XM    = 0x24
-LSM9DS0_CTRL_REG6_XM    = 0x25
-LSM9DS0_CTRL_REG7_XM    = 0x26
 
-# Accelerometer addresses
-LSM9DS0_OUT_X_L_A       = 0x28
-LSM9DS0_OUT_X_H_A       = 0x29
-LSM9DS0_OUT_Y_L_A       = 0x2A
-LSM9DS0_OUT_Y_H_A       = 0x2B
-LSM9DS0_OUT_Z_L_A       = 0x2C
-LSM9DS0_OUT_Z_H_A       = 0x2D
+class Device(object):
+    """Class for communicating with an I2C device using the adafruit-pureio pure
+    python smbus library, or other smbus compatible I2C interface. Allows reading
+    and writing 8-bit, 16-bit, and byte array values to registers
+    on the device."""
+    def __init__(self, address, busnum, i2c_interface=None):
+        """Create an instance of the I2C device at the specified address on the
+        specified I2C bus number."""
+        self._address = address
+        if i2c_interface is None:
+            # Use pure python I2C interface if none is specified.
+            import Adafruit_PureIO.smbus
+            self._bus = Adafruit_PureIO.smbus.SMBus(busnum)
+        else:
+            # Otherwise use the provided class to create an smbus interface.
+            self._bus = i2c_interface(busnum)
+        self._logger = logging.getLogger('Adafruit_I2C.Device.Bus.{0}.Address.{1:#0X}' \
+                                .format(busnum, address))
 
-# 2D accelerometer low-high register tuple
-LSM9DS0_OUT_XYZ_LH_A    = ((LSM9DS0_OUT_X_L_A, LSM9DS0_OUT_X_H_A),
-                           (LSM9DS0_OUT_Y_L_A, LSM9DS0_OUT_Y_H_A),
-                           (LSM9DS0_OUT_Z_L_A, LSM9DS0_OUT_Z_H_A))
+    def writeRaw8(self, value):
+        """Write an 8-bit value on the bus (without register)."""
+        value = value & 0xFF
+        self._bus.write_byte(self._address, value)
+        self._logger.debug("Wrote 0x%02X",
+                     value)
 
-# Various settings included in the Arduino library. I haven't used these,
-# to keep to a default setting for simplicity, however, users can change
-# the settings easily.
-LSM9DS0_ACCELRANGE_2G                = 0b000 << 3
-LSM9DS0_ACCELRANGE_4G                = 0b001 << 3
-LSM9DS0_ACCELRANGE_6G                = 0b010 << 3
-LSM9DS0_ACCELRANGE_8G                = 0b011 << 3
-LSM9DS0_ACCELRANGE_16G               = 0b100 << 3
- 
-LSM9DS0_ACCELDATARATE_POWERDOWN      = 0b0000 << 4
-LSM9DS0_ACCELDATARATE_3_125HZ        = 0b0001 << 4
-LSM9DS0_ACCELDATARATE_6_25HZ         = 0b0010 << 4
-LSM9DS0_ACCELDATARATE_12_5HZ         = 0b0011 << 4
-LSM9DS0_ACCELDATARATE_25HZ           = 0b0100 << 4
-LSM9DS0_ACCELDATARATE_50HZ           = 0b0101 << 4
-LSM9DS0_ACCELDATARATE_100HZ          = 0b0110 << 4
-LSM9DS0_ACCELDATARATE_200HZ          = 0b0111 << 4
-LSM9DS0_ACCELDATARATE_400HZ          = 0b1000 << 4
-LSM9DS0_ACCELDATARATE_800HZ          = 0b1001 << 4
-LSM9DS0_ACCELDATARATE_1600HZ         = 0b1010 << 4
- 
-LSM9DS0_MAGGAIN_2GAUSS               = 0b00 << 5
-LSM9DS0_MAGGAIN_4GAUSS               = 0b01 << 5
-LSM9DS0_MAGGAIN_8GAUSS               = 0b10 << 5
-LSM9DS0_MAGGAIN_12GAUSS              = 0b11 << 5
+    def write8(self, register, value):
+        """Write an 8-bit value to the specified register."""
+        value = value & 0xFF
+        self._bus.write_byte_data(self._address, register, value)
+        self._logger.debug("Wrote 0x%02X to register 0x%02X",
+                     value, register)
 
-LSM9DS0_MAGDATARATE_3_125HZ          = 0b000 << 2
-LSM9DS0_MAGDATARATE_6_25HZ           = 0b001 << 2
-LSM9DS0_MAGDATARATE_12_5HZ           = 0b010 << 2
-LSM9DS0_MAGDATARATE_25HZ             = 0b011 << 2
-LSM9DS0_MAGDATARATE_50HZ             = 0b100 << 2
-LSM9DS0_MAGDATARATE_100HZ            = 0b101 << 2
+    def write16(self, register, value):
+        """Write a 16-bit value to the specified register."""
+        value = value & 0xFFFF
+        self._bus.write_word_data(self._address, register, value)
+        self._logger.debug("Wrote 0x%04X to register pair 0x%02X, 0x%02X",
+                     value, register, register+1)
 
-LSM9DS0_GYROSCALE_245DPS             = 0b00 << 4
-LSM9DS0_GYROSCALE_500DPS             = 0b01 << 4
-LSM9DS0_GYROSCALE_2000DPS            = 0b10 << 4
+    def writeList(self, register, data):
+        """Write bytes to the specified register."""
+        self._bus.write_i2c_block_data(self._address, register, data)
+        self._logger.debug("Wrote to register 0x%02X: %s",
+                     register, data)
 
-class LSM9DS0(object):
-    # Debug set to false for the moment. Change to find bugs
-    def __init__(self, accel_address=LSM9DS0_ACCEL_ADDRESS,
-            mag_address=LSM9DS0_MAG_ADDRESS, gyro_address=LSM9DS0_GYRO_ADDRESS,
-            I2C=None, busnum=None):
-        """Initialise the LSM9DS0 accelerometer, magnetometer and gyroscope.
-        """
+    def readList(self, register, length):
+        """Read a length number of bytes from the specified register.  Results
+        will be returned as a bytearray."""
+        results = self._bus.read_i2c_block_data(self._address, register, length)
+        self._logger.debug("Read the following from register 0x%02X: %s",
+                     register, results)
+        return results
 
-        # Setup I2C if not already given
-        if I2C is None:
-            import Adafruit_GPIO.I2C as AdaI2C
-            I2C = AdaI2C
+    def readRaw8(self):
+        """Read an 8-bit value on the bus (without register)."""
+        result = self._bus.read_byte(self._address) & 0xFF
+        self._logger.debug("Read 0x%02X",
+                    result)
+        return result
 
-        # Each feature is given a call name. Although The magnetometer and
-        # accelerometer use the same address, they've been given different
-        # names for clarity.
-        self.mag    = I2C.get_i2c_device(mag_address, busnum)
-        self.accel  = I2C.get_i2c_device(accel_address, busnum)
-        self.gyro   = I2C.get_i2c_device(gyro_address, busnum)
+    def readU8(self, register):
+        """Read an unsigned byte from the specified register."""
+        result = self._bus.read_byte_data(self._address, register) & 0xFF
+        self._logger.debug("Read 0x%02X from register 0x%02X",
+                     result, register)
+        return result
 
-        # Magnetometer initialisation
-        self.mag.write8(LSM9DS0_CTRL_REG5_XM, 0b11110000) # Temperature sensor enabled, high res mag, 50Hz
-        self.mag.write8(LSM9DS0_CTRL_REG6_XM, 0b01100000) # +/- 12 gauss
-        self.mag.write8(LSM9DS0_CTRL_REG7_XM, 0b00000000) # Normal mode, continuous-conversion mode
+    def readS8(self, register):
+        """Read a signed byte from the specified register."""
+        result = self.readU8(register)
+        if result > 127:
+            result -= 256
+        return result
 
-        # Accelerometer initialisation
-        self.accel.write8(LSM9DS0_CTRL_REG1_XM, 0b01100111) # 100Hz, XYZ enabled
-        self.accel.write8(LSM9DS0_CTRL_REG2_XM, 0b00100000) # +/- 16 g
+    def readU16(self, register, little_endian=True):
+        """Read an unsigned 16-bit value from the specified register, with the
+        specified endianness (default little endian, or least significant byte
+        first)."""
+        result = self._bus.read_word_data(self._address,register) & 0xFFFF
+        self._logger.debug("Read 0x%04X from register pair 0x%02X, 0x%02X",
+                           result, register, register+1)
+        # Swap bytes if using big endian because read_word_data assumes little
+        # endian on ARM (little endian) systems.
+        if not little_endian:
+            result = ((result << 8) & 0xFF00) + (result >> 8)
+        return result
 
-        # Gyro initialisation
-        self.gyro.write8(LSM9DS0_CTRL_REG1_G, 0b00001111) # Normal power mode, XYZ enabled
-        self.gyro.write8(LSM9DS0_CTRL_REG4_G, 0b00110000) # Continuous update, 2000 dps
+    def readS16(self, register, little_endian=True):
+        """Read a signed 16-bit value from the specified register, with the
+        specified endianness (default little endian, or least significant byte
+        first)."""
+        result = self.readU16(register, little_endian)
+        if result > 32767:
+            result -= 65536
+        return result
 
-    def readLowHigh(self, i2c_device, lowhigh):
-        """Returns signed integer value by reading the given sensor's low and high
-        bytes.
-        """
-        # Unpack low high register pair
-        (low, high) = lowhigh
+    def readU16LE(self, register):
+        """Read an unsigned 16-bit value from the specified register, in little
+        endian byte order."""
+        return self.readU16(register, little_endian=True)
 
-        # Combine the low and high bytes to create new binary value
-        # Note the initial 'reading' is postive, so must be converted
-        reading = i2c_device.readU8(low) | i2c_device.readU8(high) << 8
+    def readU16BE(self, register):
+        """Read an unsigned 16-bit value from the specified register, in big
+        endian byte order."""
+        return self.readU16(register, little_endian=False)
 
-        # Convert to negative value if necessary
-        if reading > 32767:
-            reading -= 65536
+    def readS16LE(self, register):
+        """Read a signed 16-bit value from the specified register, in little
+        endian byte order."""
+        return self.readS16(register, little_endian=True)
 
-        return reading
-
-    def readSensor(self, i2c_device, xyz_lh):
-        """Returns (x, y, z) tuple from the given sensor's registers
-        """
-        xyz = (self.readLowHigh(i2c_device, xyz_lh[0]),  # Pass x lowhigh registers
-               self.readLowHigh(i2c_device, xyz_lh[1]),  # Pass y
-               self.readLowHigh(i2c_device, xyz_lh[2]))  # Pass z
-
-        return xyz
-    
-    def readGyro(self):
-        """Return gyroscope (x, y, z) tuple"""
-
-        return self.readSensor(self.gyro, LSM9DS0_OUT_XYZ_LH_G)
-
-    def readMag(self):
-        """Return magnetometer (x, y, z) tuple"""
-
-        return self.readSensor(self.mag, LSM9DS0_OUT_XYZ_LH_M)
-
-    def readAccel(self):
-        """Returns accelerometer (x, y, z) tuple"""
-
-        return self.readSensor(self.accel, LSM9DS0_OUT_XYZ_LH_A)
-
-    def read(self):
-        """Returns tuple of (gyroxyz, magxyz, accelxyz)"""
-
-        return (self.readGyro(), self.readMag(), self.readAccel())
-
-    # The documentation on reading temperature is not very clear, and it appears
-    # that the sensor does not provide an ambient temperature reading, with no
-    # absolute value, instead measuring change in temp inside the chip
-    def rawTemp(self):
-        """Returns temperature value (WARNING: not ambient temperature)"""
-
-        # The temperature values are read from mag/ accel device
-        return self.readSensor(self.mag, LSM9DS0_OUT_TEMP_LH)
+    def readS16BE(self, register):
+        """Read a signed 16-bit value from the specified register, in big
+        endian byte order."""
+        return self.readS16(register, little_endian=False)
