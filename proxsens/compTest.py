c12a0fde199b81362664c2f98193f618ec235716
Avi <USER@DESKTOP-L7GO5KG>
2017-03-13 17:41:09 +0200

    sf

--- proxsens/compTest.py
+++ proxsens/compTest.py
@@ -28,113 +28,114 @@ import math
 
 # The same address is used for both the magnetometer and accelerometer, but
 # each has their own variable, to avoid confusion.
-LSM9DS0_MAG_ADDRESS     = 0x1D
-LSM9DS0_ACCEL_ADDRESS	= 0x1D
-LSM9DS0_GYRO_ADDRESS    = 0x6B
-                                                                                
+LSM9DS0_MAG_ADDRESS = 0x1D
+LSM9DS0_ACCEL_ADDRESS = 0x1D
+LSM9DS0_GYRO_ADDRESS = 0x6B
+
 # LSM9DS0 gyrometer registers
-LSM9DS0_WHO_AM_I_G      = 0x0F
-LSM9DS0_CTRL_REG1_G     = 0x20
-LSM9DS0_CTRL_REG3_G     = 0x22
-LSM9DS0_CTRL_REG4_G     = 0x23
-LSM9DS0_OUT_X_L_G       = 0x28
-LSM9DS0_OUT_X_H_G       = 0x29
-LSM9DS0_OUT_Y_L_G       = 0x2A
-LSM9DS0_OUT_Y_H_G       = 0x2B
-LSM9DS0_OUT_Z_L_G       = 0x2C
-LSM9DS0_OUT_Z_H_G       = 0x2D
+LSM9DS0_WHO_AM_I_G = 0x0F
+LSM9DS0_CTRL_REG1_G = 0x20
+LSM9DS0_CTRL_REG3_G = 0x22
+LSM9DS0_CTRL_REG4_G = 0x23
+LSM9DS0_OUT_X_L_G = 0x28
+LSM9DS0_OUT_X_H_G = 0x29
+LSM9DS0_OUT_Y_L_G = 0x2A
+LSM9DS0_OUT_Y_H_G = 0x2B
+LSM9DS0_OUT_Z_L_G = 0x2C
+LSM9DS0_OUT_Z_H_G = 0x2D
 
 # 2D gyroscope low-high register tuple
-LSM9DS0_OUT_XYZ_LH_G    = ((LSM9DS0_OUT_X_L_G, LSM9DS0_OUT_X_H_G),
-                           (LSM9DS0_OUT_Y_L_G, LSM9DS0_OUT_Y_H_G),
-                           (LSM9DS0_OUT_Z_L_G, LSM9DS0_OUT_Z_H_G))
+LSM9DS0_OUT_XYZ_LH_G = ((LSM9DS0_OUT_X_L_G, LSM9DS0_OUT_X_H_G),
+                        (LSM9DS0_OUT_Y_L_G, LSM9DS0_OUT_Y_H_G),
+                        (LSM9DS0_OUT_Z_L_G, LSM9DS0_OUT_Z_H_G))
 
 # LSM9DS0 temperature addresses
-LSM9DS0_OUT_TEMP_L_XM   = 0x05
-LSM9DS0_OUT_TEMP_H_XM   = 0x06
+LSM9DS0_OUT_TEMP_L_XM = 0x05
+LSM9DS0_OUT_TEMP_H_XM = 0x06
 
 # Temperature low-high register tuple
 LSM9DS0_OUT_TEMP_LH = (LSM9DS0_OUT_TEMP_L_XM, LSM9DS0_OUT_TEMP_H_XM)
 
 # Magnetometer addresses
-LSM9DS0_STATUS_REG_M    = 0x07
-LSM9DS0_OUT_X_L_M       = 0x08
-LSM9DS0_OUT_X_H_M       = 0x09
-LSM9DS0_OUT_Y_L_M       = 0x0A
-LSM9DS0_OUT_Y_H_M       = 0x0B
-LSM9DS0_OUT_Z_L_M       = 0x0C
-LSM9DS0_OUT_Z_H_M       = 0x0D
+LSM9DS0_STATUS_REG_M = 0x07
+LSM9DS0_OUT_X_L_M = 0x08
+LSM9DS0_OUT_X_H_M = 0x09
+LSM9DS0_OUT_Y_L_M = 0x0A
+LSM9DS0_OUT_Y_H_M = 0x0B
+LSM9DS0_OUT_Z_L_M = 0x0C
+LSM9DS0_OUT_Z_H_M = 0x0D
 
 # 2D magnetometer low-high register tuple
-LSM9DS0_OUT_XYZ_LH_M    = ((LSM9DS0_OUT_X_L_M, LSM9DS0_OUT_X_H_M),
-                           (LSM9DS0_OUT_Y_L_M, LSM9DS0_OUT_Y_H_M),
-                           (LSM9DS0_OUT_Z_L_M, LSM9DS0_OUT_Z_H_M))
+LSM9DS0_OUT_XYZ_LH_M = ((LSM9DS0_OUT_X_L_M, LSM9DS0_OUT_X_H_M),
+                        (LSM9DS0_OUT_Y_L_M, LSM9DS0_OUT_Y_H_M),
+                        (LSM9DS0_OUT_Z_L_M, LSM9DS0_OUT_Z_H_M))
 
 # Shared (mag and accel) addresses
-LSM9DS0_WHO_AM_I_XM     = 0x0F
-LSM9DS0_INT_CTRL_REG_M  = 0x12
-LSM9DS0_INT_SRC_REG_M   = 0x13
-LSM9DS0_CTRL_REG1_XM    = 0x20
-LSM9DS0_CTRL_REG2_XM    = 0x21
-LSM9DS0_CTRL_REG5_XM    = 0x24
-LSM9DS0_CTRL_REG6_XM    = 0x25
-LSM9DS0_CTRL_REG7_XM    = 0x26
+LSM9DS0_WHO_AM_I_XM = 0x0F
+LSM9DS0_INT_CTRL_REG_M = 0x12
+LSM9DS0_INT_SRC_REG_M = 0x13
+LSM9DS0_CTRL_REG1_XM = 0x20
+LSM9DS0_CTRL_REG2_XM = 0x21
+LSM9DS0_CTRL_REG5_XM = 0x24
+LSM9DS0_CTRL_REG6_XM = 0x25
+LSM9DS0_CTRL_REG7_XM = 0x26
 
 # Accelerometer addresses
-LSM9DS0_OUT_X_L_A       = 0x28
-LSM9DS0_OUT_X_H_A       = 0x29
-LSM9DS0_OUT_Y_L_A       = 0x2A
-LSM9DS0_OUT_Y_H_A       = 0x2B
-LSM9DS0_OUT_Z_L_A       = 0x2C
-LSM9DS0_OUT_Z_H_A       = 0x2D
+LSM9DS0_OUT_X_L_A = 0x28
+LSM9DS0_OUT_X_H_A = 0x29
+LSM9DS0_OUT_Y_L_A = 0x2A
+LSM9DS0_OUT_Y_H_A = 0x2B
+LSM9DS0_OUT_Z_L_A = 0x2C
+LSM9DS0_OUT_Z_H_A = 0x2D
 
 # 2D accelerometer low-high register tuple
-LSM9DS0_OUT_XYZ_LH_A    = ((LSM9DS0_OUT_X_L_A, LSM9DS0_OUT_X_H_A),
-                           (LSM9DS0_OUT_Y_L_A, LSM9DS0_OUT_Y_H_A),
-                           (LSM9DS0_OUT_Z_L_A, LSM9DS0_OUT_Z_H_A))
+LSM9DS0_OUT_XYZ_LH_A = ((LSM9DS0_OUT_X_L_A, LSM9DS0_OUT_X_H_A),
+                        (LSM9DS0_OUT_Y_L_A, LSM9DS0_OUT_Y_H_A),
+                        (LSM9DS0_OUT_Z_L_A, LSM9DS0_OUT_Z_H_A))
 
 # Various settings included in the Arduino library. I haven't used these,
 # to keep to a default setting for simplicity, however, users can change
 # the settings easily.
-LSM9DS0_ACCELRANGE_2G                = 0b000 << 3
-LSM9DS0_ACCELRANGE_4G                = 0b001 << 3
-LSM9DS0_ACCELRANGE_6G                = 0b010 << 3
-LSM9DS0_ACCELRANGE_8G                = 0b011 << 3
-LSM9DS0_ACCELRANGE_16G               = 0b100 << 3
- 
-LSM9DS0_ACCELDATARATE_POWERDOWN      = 0b0000 << 4
-LSM9DS0_ACCELDATARATE_3_125HZ        = 0b0001 << 4
-LSM9DS0_ACCELDATARATE_6_25HZ         = 0b0010 << 4
-LSM9DS0_ACCELDATARATE_12_5HZ         = 0b0011 << 4
-LSM9DS0_ACCELDATARATE_25HZ           = 0b0100 << 4
-LSM9DS0_ACCELDATARATE_50HZ           = 0b0101 << 4
-LSM9DS0_ACCELDATARATE_100HZ          = 0b0110 << 4
-LSM9DS0_ACCELDATARATE_200HZ          = 0b0111 << 4
-LSM9DS0_ACCELDATARATE_400HZ          = 0b1000 << 4
-LSM9DS0_ACCELDATARATE_800HZ          = 0b1001 << 4
-LSM9DS0_ACCELDATARATE_1600HZ         = 0b1010 << 4
- 
-LSM9DS0_MAGGAIN_2GAUSS               = 0b00 << 5
-LSM9DS0_MAGGAIN_4GAUSS               = 0b01 << 5
-LSM9DS0_MAGGAIN_8GAUSS               = 0b10 << 5
-LSM9DS0_MAGGAIN_12GAUSS              = 0b11 << 5
+LSM9DS0_ACCELRANGE_2G = 0b000 << 3
+LSM9DS0_ACCELRANGE_4G = 0b001 << 3
+LSM9DS0_ACCELRANGE_6G = 0b010 << 3
+LSM9DS0_ACCELRANGE_8G = 0b011 << 3
+LSM9DS0_ACCELRANGE_16G = 0b100 << 3
 
-LSM9DS0_MAGDATARATE_3_125HZ          = 0b000 << 2
-LSM9DS0_MAGDATARATE_6_25HZ           = 0b001 << 2
-LSM9DS0_MAGDATARATE_12_5HZ           = 0b010 << 2
-LSM9DS0_MAGDATARATE_25HZ             = 0b011 << 2
-LSM9DS0_MAGDATARATE_50HZ             = 0b100 << 2
-LSM9DS0_MAGDATARATE_100HZ            = 0b101 << 2
+LSM9DS0_ACCELDATARATE_POWERDOWN = 0b0000 << 4
+LSM9DS0_ACCELDATARATE_3_125HZ = 0b0001 << 4
+LSM9DS0_ACCELDATARATE_6_25HZ = 0b0010 << 4
+LSM9DS0_ACCELDATARATE_12_5HZ = 0b0011 << 4
+LSM9DS0_ACCELDATARATE_25HZ = 0b0100 << 4
+LSM9DS0_ACCELDATARATE_50HZ = 0b0101 << 4
+LSM9DS0_ACCELDATARATE_100HZ = 0b0110 << 4
+LSM9DS0_ACCELDATARATE_200HZ = 0b0111 << 4
+LSM9DS0_ACCELDATARATE_400HZ = 0b1000 << 4
+LSM9DS0_ACCELDATARATE_800HZ = 0b1001 << 4
+LSM9DS0_ACCELDATARATE_1600HZ = 0b1010 << 4
 
-LSM9DS0_GYROSCALE_245DPS             = 0b00 << 4
-LSM9DS0_GYROSCALE_500DPS             = 0b01 << 4
-LSM9DS0_GYROSCALE_2000DPS            = 0b10 << 4
+LSM9DS0_MAGGAIN_2GAUSS = 0b00 << 5
+LSM9DS0_MAGGAIN_4GAUSS = 0b01 << 5
+LSM9DS0_MAGGAIN_8GAUSS = 0b10 << 5
+LSM9DS0_MAGGAIN_12GAUSS = 0b11 << 5
+
+LSM9DS0_MAGDATARATE_3_125HZ = 0b000 << 2
+LSM9DS0_MAGDATARATE_6_25HZ = 0b001 << 2
+LSM9DS0_MAGDATARATE_12_5HZ = 0b010 << 2
+LSM9DS0_MAGDATARATE_25HZ = 0b011 << 2
+LSM9DS0_MAGDATARATE_50HZ = 0b100 << 2
+LSM9DS0_MAGDATARATE_100HZ = 0b101 << 2
+
+LSM9DS0_GYROSCALE_245DPS = 0b00 << 4
+LSM9DS0_GYROSCALE_500DPS = 0b01 << 4
+LSM9DS0_GYROSCALE_2000DPS = 0b10 << 4
+
 
 class LSM9DS0(object):
     # Debug set to false for the moment. Change to find bugs
     def __init__(self, accel_address=LSM9DS0_ACCEL_ADDRESS,
-            mag_address=LSM9DS0_MAG_ADDRESS, gyro_address=LSM9DS0_GYRO_ADDRESS,
-            I2C=None, busnum=None):
+                 mag_address=LSM9DS0_MAG_ADDRESS, gyro_address=LSM9DS0_GYRO_ADDRESS,
+                 I2C=None, busnum=None):
         """Initialise the LSM9DS0 accelerometer, magnetometer and gyroscope.
         """
 
@@ -146,22 +147,22 @@ class LSM9DS0(object):
         # Each feature is given a call name. Although The magnetometer and
         # accelerometer use the same address, they've been given different
         # names for clarity.
-        self.mag    = I2C.get_i2c_device(mag_address, busnum)
-        self.accel  = I2C.get_i2c_device(accel_address, busnum)
-        self.gyro   = I2C.get_i2c_device(gyro_address, busnum)
+        self.mag = I2C.get_i2c_device(mag_address, busnum)
+        self.accel = I2C.get_i2c_device(accel_address, busnum)
+        self.gyro = I2C.get_i2c_device(gyro_address, busnum)
 
         # Magnetometer initialisation
-        self.mag.write8(LSM9DS0_CTRL_REG5_XM, 0b11110000) # Temperature sensor enabled, high res mag, 50Hz
-        self.mag.write8(LSM9DS0_CTRL_REG6_XM, 0b01100000) # +/- 12 gauss
-        self.mag.write8(LSM9DS0_CTRL_REG7_XM, 0b00000000) # Normal mode, continuous-conversion mode
+        self.mag.write8(LSM9DS0_CTRL_REG5_XM, 0b11110000)  # Temperature sensor enabled, high res mag, 50Hz
+        self.mag.write8(LSM9DS0_CTRL_REG6_XM, 0b01100000)  # +/- 12 gauss
+        self.mag.write8(LSM9DS0_CTRL_REG7_XM, 0b00000000)  # Normal mode, continuous-conversion mode
 
         # Accelerometer initialisation
-        self.accel.write8(LSM9DS0_CTRL_REG1_XM, 0b01100111) # 100Hz, XYZ enabled
-        self.accel.write8(LSM9DS0_CTRL_REG2_XM, 0b00100000) # +/- 16 g
+        self.accel.write8(LSM9DS0_CTRL_REG1_XM, 0b01100111)  # 100Hz, XYZ enabled
+        self.accel.write8(LSM9DS0_CTRL_REG2_XM, 0b00100000)  # +/- 16 g
 
         # Gyro initialisation
-        self.gyro.write8(LSM9DS0_CTRL_REG1_G, 0b00001111) # Normal power mode, XYZ enabled
-        self.gyro.write8(LSM9DS0_CTRL_REG4_G, 0b00110000) # Continuous update, 2000 dps
+        self.gyro.write8(LSM9DS0_CTRL_REG1_G, 0b00001111)  # Normal power mode, XYZ enabled
+        self.gyro.write8(LSM9DS0_CTRL_REG4_G, 0b00110000)  # Continuous update, 2000 dps
 
     def readLowHigh(self, i2c_device, lowhigh):
         """Returns signed integer value by reading the given sensor's low and high
@@ -180,6 +181,24 @@ class LSM9DS0(object):
 
         return reading
 
+    def heading(self):
+
+        x, y, z = self.readMag()
+        headingRad = math.atan2(x, y)
+        headingRad += self.__declination
+
+        # Correct for reversed heading
+        if headingRad < 0:
+            headingRad += 2 * math.pi
+
+        # Check for wrap and compensate
+        elif headingRad > 2 * math.pi:
+            headingRad -= 2 * math.pi
+
+        # Convert to degrees from radians
+        headingDeg = headingRad * 180 / math.pi
+        return headingDeg
+
     def readSensor(self, i2c_device, xyz_lh):
         """Returns (x, y, z) tuple from the given sensor's registers
         """
@@ -188,7 +207,7 @@ class LSM9DS0(object):
                self.readLowHigh(i2c_device, xyz_lh[2]))  # Pass z
 
         return xyz
-    
+
     def readGyro(self):
         """Return gyroscope (x, y, z) tuple"""
 
