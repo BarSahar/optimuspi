a98a7d3f7f7abfa99be7fed25fac1162cd4cb9eb
Avi <USER@DESKTOP-L7GO5KG>
2017-03-13 17:05:41 +0200

    s

--- proxsens/compTest.py
+++ proxsens/compTest.py
@@ -140,7 +140,7 @@ class LSM9DS0(object):
 
         # Setup I2C if not already given
         if I2C is None:
-            import Adafruit_GPIO as AdaI2C
+            import adafruitSmbus
             I2C = AdaI2C
 
         # Each feature is given a call name. Although The magnetometer and
