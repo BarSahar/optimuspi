992776b4698c4b582176787c66d17137d54e65d5
Avi Hazan <avihazan22@gmail.com>
2017-04-02 17:53:16 +0300

    PWM pin of the Raspberry Pi

--- proxsens/proxsens.py
+++ proxsens/proxsens.py
@@ -239,7 +239,7 @@ def moveForwardMicro():
     global HeadingAngle
     globalinit()
     counterright_limit = 50
-    counterleft_limit = 50
+    counterleft_limit = 45
     HeadingAngle = getCompRead()
     print("HeadingAngle: " + str(HeadingAngle))
     GPIO.output(A1, False)
