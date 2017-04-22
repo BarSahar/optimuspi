fed84f271bd64cee75b63b90414e0c126ab595d5
Bar <Bar@DESKTOP-7Q2EJ8D>
2017-04-22 11:57:12 +0300

    asd

--- proxsens/server.py
+++ proxsens/server.py
@@ -131,8 +131,10 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             filePath = str(os.getcwd())+'/patrolPics'
-            num_files = len([f for f in os.listdir(filePath) if os.path.isfile(os.path.join(filePath, f))])
-            self.wfile.write(str(num_files).encode())
+            #os.path.realpath(__file__)
+            #num_files = len([f for f in os.listdir(filePath) if os.path.isfile(os.path.join(filePath, f))])
+            #self.wfile.write(str(num_files).encode())
+            self.wfile.write(filePath .encode())
             return
         elif "getPic" in self.path:
             import os.path
