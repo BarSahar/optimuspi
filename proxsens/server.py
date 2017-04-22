2792ac1ac6ab564d9dfacff83c9614bd6c1a08e4
Bar <Bar@DESKTOP-7Q2EJ8D>
2017-04-22 15:30:33 +0300

    asd

--- proxsens/server.py
+++ proxsens/server.py
@@ -139,11 +139,11 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            filePath = str(os.getcwd()) + '/patrolPics/pic' + str(str.split("pic")[1])
-            #imageFile = open(filePath, "rb")
-            #imageBase64Str = str(base64.b64encode(imageFile.read()))[2:-1]
-            #self.wfile.write(imageBase64Str.encode())
-            self.wfile.write(filePath.encode())
+            filePath = str(os.getcwd()) + '/patrolPics/pic' + str(self.path.split("pic")[1])
+            imageFile = open(filePath, "rb")
+            imageBase64Str = str(base64.b64encode(imageFile.read()))[2:-1]
+            self.wfile.write(imageBase64Str.encode())
+            #self.wfile.write(filePath.encode())
             return
 
 
