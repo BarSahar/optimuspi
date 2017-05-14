a2530c3c026735880c0bc063ddf050d4d3e9157f
Avi Hazan <avihazan22@gmail.com>
2017-05-14 15:42:04 +0300

    c

--- proxsens/server.py
+++ proxsens/server.py
@@ -29,10 +29,10 @@ def createScript():
         # a = subprocess.Popen("chmod +x  raspivid -n -ih -t 0 -rot 0 -w 1280 -h 720 -fps 30 -b 1000000 -o - | nc -lkv4 5001",shell=True)
         # a= subprocess.call('./data.sh',shell=True,timeout=None)
 
-        a= subprocess.run('python3 startet.py',shell=True,timeout=None)
+        # a= subprocess.run('python3 startet.py',shell=True,timeout=None)
 
 
-        # os.system("chmod +x data.sh")
+        os.system("chmod +x startet.py")
         # os.system("./data.sh")
 
         # os.system("python3 startet.py")
